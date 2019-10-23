f2b1cf6b5c97376225802da794d1b8ca10880a4f
WENI HSU <hsu.weni@gmail.com>
2019-10-23 10:42:17 +0800

    版本檢查

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import subprocess
 import sys
 import pymysql.cursors
 import yaml
@@ -15,6 +15,8 @@ oldString = cfg['setup']['old_string']
 newString = cfg['setup']['new_string']
 tableList = cfg['setup']['table_list']
 excludeTableList = cfg['setup']['exclude_table_list']
+git_remote = 'git@github.com:dkben/change-sql-all-table-row-column-string.git'
+
 
 # Connect to the database
 connection = pymysql.connect(host=host,
@@ -50,6 +52,9 @@ def update(table_name, column_list):
 
 
 def main():
+    print("============================================================")
+    check_update()
+
     if len(tableList) == 0:
         get_tables()
     print("all tables :", tableList)
@@ -64,6 +69,23 @@ def main():
             if len(column_list) > 0:
                 print(table_name, ":", column_list)
                 update(table_name, column_list)
+
+
+def check_update():
+    check = str(input('是否檢查更新？y 檢查更新 / n 繼續...[y/n]:'))
+    if check == 'y':
+        print('檢查更新中...')
+        local_hash = subprocess.check_output('git log --pretty="%h" -n1 HEAD'.split()).decode()[1:8]
+        remote_hash = subprocess.check_output(
+            ('git ls-remote %s HEAD' % git_remote).split()).decode()[0:7]
+        print(local_hash, remote_hash)
+        if local_hash != remote_hash:
+            skip = str(input('有新版程式，請 git pull 更新...，按 y 繼續 / n 離開程式...[y/n]:'))
+            if skip == 'n':
+                sys.exit()
+        else:
+            input('已經是最新版...按任意鍵繼續...')
+
 
 
 if __name__ == '__main__':
